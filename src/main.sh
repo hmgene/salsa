87f6686835c93c2cf71eeef4dda7df1865a78f42
Hyunmin Kim <oannes@Hyunmins-MacBook.local>
2019-05-16 15:05:26 -0600

    install

--- src/main.sh
+++ src/main.sh
@@ -1,75 +1,58 @@
-salsa.junction(){
+
+R(){
 usage="
-$FUNCNAME <bed12>
-";if [ $# -lt 1 ];then echo "$usage"; return; fi
-cat $1 | perl -e 'use strict; my %res=();
-	while(<STDIN>){ chomp; my @a=split/\t/,$_;
-		print $_,"\n"; 
+$FUNCNAME <sam|bam>
+"
+if [ $# -lt 1 ];then echo "$usage"; return; fi
+        samtools view -hb $@ | bedtools bamtobed -bed12 -i stdin
+}
+
+J(){ usage="
+$FUNCNAME <sam|bam> 
+"; if [ $# -lt 1 ];then echo "$usage"; return; fi
+	R $1 | bed12_to_eej > $2.eej
+	bedtools intersect -a $2.eij -b $1 -wa -wb
+}
+
+J.test(){
+echo "chr1	100	200	n	0	+	100	200	0	2	10,20	0,80
+chr1	101	201	n	0	+	101	201	0	2	9,21	0,79
+chr1	100	200	n	0	+	100	200	0	1	100	0
+" | bed12_to_eej - 	
+}
+
+unspliced(){
+usage="
+$FUNCNAME <splice_junction> <read.bed12>
+"; if [ $# -ne 2 ];then echo "$usage"; return; fi
+
+		
+}
+
+bed12_to_eej(){
+usage="
+$FUNCNAME <read.bed12>
+"
+if [ $# -ne 1 ];then echo "$usage"; return; fi
+	perl -e 'my %r=();
+	while(<STDIN>){chomp;my @a=split/\t/,$_;
 		my @l=split/,/,$a[10];
 		my @s=split/,/,$a[11];
-		for(my $i=0; $i < $a[9] - 1; $i++){
-			my $start= $a[1] + $s[$i] + $l[$i] - 1;
-			my $end = $a[1] + $s[$i+1] + 1;
-			$res{$a[5]}{$a[0]}{$start."\t".$end} ++;
-		}
+		map { 
+			my @b=map { $a[1] + $_ } ( $s[$_]+$l[$_], $s[$_+1] );
+			$r{ join("\t",$a[0],$a[1]+$s[$_]+$l[$_],$a[1]+$s[$_+1]) }{ $l[$_]."&".$l[$_+1] } ++ ;
+		} 0..($a[9]-2);
 	}
-	foreach my $st (keys %res){
-	foreach my $c (keys %{$res{$st}}){
-	foreach my $se (keys %{$res{$st}{$c}}){
-		print join("\t",( $c,$se,"j",$res{$st}{$c}{$se},$st)),"\n";
-	}}}
-'
-} 
-salsa.junction.test(){
-echo "
-01234567890123456789
- EEEE-EEE---EEEEEE
-" | salsa gen_bed12 - | salsa junction -
+	foreach my $k (keys %r){
+		print $k,"\t";
+		print join(",", map { $_."=".$r{$k}{$_} } keys %{$r{$k}} ),"\n";
+	}
+	'
 }
 
-salsa.gen_bed12(){
-usage="$FUNCNAME <intput.txt>"
-if [ $# -lt 1 ];then echo "$usage"; return; fi
-cat $1 | perl -e 'use strict; 
-	sub f{
-		my ($s,$e,$g) = @_;
-		my $n=scalar @$s;
-		#print join(",",@$s)," ",join(",",@$e),"\n";
-		my $s0=$s->[0];		
-		my $e0=$e->[$n-1];		
-		my $st="+"; if( lc $g eq $g){ $st="-";}
-		print "chr1\t$s0\t$e0\t$g\t0\t$st\t$s0\t$e0\t0,0,0\t$n\t";
-		print join(",",map { $e->[$_] - $s->[$_]} (0..($n-1))),"\t";
-		print join(",",map { $_ - $s0 } @$s),"\n";
-
-	}
-	while(<STDIN>){chomp;  my $tmp=" ".$_." "; $tmp=~s/\d/ /g;
-		my @S=split//,$tmp;
-		my @sizes=(); my @starts=(); my @ends=();
-		my $type="";
-		my $start=0;
-		for(my $i=1; $i< scalar @S; $i++){ 
-			my ($a,$b)=($S[$i-1],$S[$i]);
-			if($a eq " " && $b =~/\w/){
-				push @starts,$i-1;
-			}elsif($a =~/\w/ && $b eq " "){
-				push @ends,$i-1;
-				f(\@starts,\@ends,$a);
-				@starts=();@ends=();
-			}elsif($a eq "-" && $b =~/\w/){
-				push @starts,$i-1;
-			}elsif($a =~/\w/ && $b eq "-"){
-				push @ends,$i-1;
-			}elsif($a ne $b){
-				push @ends,$i-1;
-				f(\@starts,\@ends,$a);
-				@starts=();@ends=();
-				push @starts,$i-1;
-			} 
-		}
-	}
-'
+bed12_to_eej__test(){
+echo "chr1	100	200	n	0	+	100	200	0	2	10,20	0,80
+chr1	101	201	n	0	+	101	201	0	2	9,21	0,79
+chr1	100	200	n	0	+	100	200	0	1	100	0
+" | bed12_to_eej -	
 }
-
-
-
