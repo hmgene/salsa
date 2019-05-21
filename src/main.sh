bcbc5ea503b8f51b9ee368a6c4fa8ba7c9272a95
Hyunmin Kim <oannes@Hyunmins-MacBook.local>
2019-05-21 17:06:21 -0600

    junction

--- src/main.sh
+++ src/main.sh
@@ -1,4 +1,102 @@
 
+sam_to_bed12(){
+usage="
+$FUNCNAME [options] <bam> [region]
+"; if [ $# -lt 1 ];then echo "$usage";return; fi
+        samtools view -hb "$@" | bedtools bamtobed -bed12 -i stdin
+}
+
+
+count_junction(){
+usage="
+$FUNCNAME <bed12>
+"; if [ $# -lt 1 ];then echo "$usage";return; fi
+   cat $1 | perl -e 'my %r=();
+        while(<STDIN>){chomp;my @a=split/\t/,$_;
+                my @l=split/,/,$a[10]; my @s=split/,/,$a[11];
+                map {
+                        $r{ $a[0] }{ $a[1]+$s[$_]+$l[$_] }{ $a[1]+$s[$_+1] } ++;
+                } 0..($a[9]-2);
+        }
+        foreach my $c (keys %r){
+	foreach my $s (keys %{$r{$c}}){
+	foreach my $e (keys %{$r{$c}{$s}}){
+                print join("\t",$c,$s,$e,$r{$c}{$s}{$e}),"\n";
+        }}}
+        '
+
+}
+count_junction__test(){
+echo "chr1	100	1000	g1	0	+	100	1000	0	3	100,200,300	0,200,600" \
+| count_junction -
+}
+
+fragment(){
+usage="$FUNCNAME <bed>"
+if [ $# -lt 1 ];then echo "$usage $@"; return; fi
+cat $1 | perl -e 'use strict;
+	my %r=();
+	while(<STDIN>){chomp;my@d=split/\t/,$_;
+		$r{$d[0]}{$d[1]} |= 1;	
+		$r{$d[0]}{$d[2]} |= 2;	
+	}
+	foreach my $c (keys %r){
+		my @x=sort {$a<=>$b} keys %{$r{$c}};
+		map{
+			print $x[$_-1]," ",$x[$_],"\n";
+		} 1..$#x;	
+	}
+'
+
+}
+fragment__test(){
+echo "chr1	100	200	g1	1	+
+chr1	300	400	g1	1	+
+chr1	150	350	g1:intron	1	+" \
+| fragment -	
+}
+
+sim(){
+usage="$FUNCNAME <bed12>"
+if [ $# -lt 1 ];then echo "$usage $@"; return; fi
+	cat $1 | perl -e 'use strict;
+		my $l_read=50;
+		my $n_read=100;
+		while(<STDIN>){ chomp; my@d=split/\t/,$_;
+			## get a transcript
+			my @l=split/,/,$d[10]; 
+			my @ll=(0,@l[0..($#l-1)]);
+			map{$ll[$_]+=$ll[$_-1];} 1..$#ll;
+			my @s=split/,/,$d[11];		
+
+			## generate reads
+			my @r_s=sort {$a<=>$b} map { int(rand($ll[$#ll]-$l_read)) } 0..$n_read;
+			my @r_e=map { $_+$l_read } @r_s;
+
+			## map to genomic positions
+			my %h=(); my $i=0; map{
+				$h{ $_ } = $i; ## exon number 
+				print "$i $_\n";
+				if($_ >= $ll[$i] ){ $i++;}
+			} sort {$a<=>$b} (@s, @r_s, @r_e);
+
+			map {	
+				my ($s1,$e1)=($r_s[$_],$r_e[$_]);
+				map{ 
+					my $s2=$d[1]+$s[$_]+($s1-$ll[$_]);
+					#print "$_:$ll[$_] $s1 -> $s2\n";
+				} $h{$s1};
+			} 0..$#r_s;
+
+		}
+		
+	'
+}
+sim__test(){
+echo "chr1	100	1000	g1	0	+	100	1000	0	3	100,200,300	0,200,600" \
+| sim -
+}
+
 R(){
 usage="
 $FUNCNAME <sam|bam>
